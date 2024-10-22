fea4ca8179177d1fc2d4a775bfc2e7b8569e261c
wagh <wagh1.2.3.002@gmail.com>
2024-10-22 16:55:04 +0530

    miner chanes

--- src/app/model/wishlist.ts
+++ src/app/model/wishlist.ts
@@ -1,13 +1,13 @@
 import mongoose, { Schema, Document } from "mongoose";
 
-export interface Wishlist extends Document {
+export interface Favourite extends Document {
   userId: mongoose.Types.ObjectId;
   vehicleId: mongoose.Types.ObjectId;
   createdAt: Date;
   updatedAt: Date;
 }
 
-const WishlistSchema: Schema<Wishlist> = new Schema({
+const FavouriteSchema: Schema<Favourite> = new Schema({
   userId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
@@ -22,5 +22,5 @@ const WishlistSchema: Schema<Wishlist> = new Schema({
   { timestamps: true }
 );
 
-const WishlistModel = mongoose.models.Wishlist || mongoose.model<Wishlist>("Wishlist", WishlistSchema);
-export default WishlistModel;
+const FavouriteModel = mongoose.models.Favourite || mongoose.model<Favourite>("Favourite", FavouriteSchema);
+export default FavouriteModel;
