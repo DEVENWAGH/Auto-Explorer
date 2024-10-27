391a6d3f997015643867edcd1357df5153bef6fe
wagh <wagh1.2.3.002@gmail.com>
2024-10-27 18:53:07 +0530

    update the packages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <div className="flex h-screen items-center justify-center text-2xl  w-full sm:p-20 font-[gilroy]">
       <main className="flex flex-col items-center row-start-2 gap-8 sm:items-start">
         <div>
-          <h1>hello wagh</h1>
+          <h1>Dev wagh</h1>
         </div>
       </main>
     </div>
